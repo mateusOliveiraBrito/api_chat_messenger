b08b4f21a13667617f099f0f59ed7a9d41ac85c2
MATEUS OLIVEIRA BRITO <moliveirabrito.institucional@gmail.com>
2022-08-03 11:11:19 -0300

    Remove comentários e usings desnecessários. Adiciona uso de contexto de banco de dados e configura banco de dados SQLite

--- api_chat_messenger/Startup.cs
+++ api_chat_messenger/Startup.cs
@@ -1,15 +1,10 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using api_chat_messenger.Database;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace api_chat_messenger {
     public class Startup {
@@ -19,18 +14,19 @@ namespace api_chat_messenger {
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
+            services.AddDbContext<ChatMessengerDatabaseContext>(config => {
+                config.UseSqlite("Data Source=Database\\ZapWeb.db");
+            });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
             if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
             }
             else {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
