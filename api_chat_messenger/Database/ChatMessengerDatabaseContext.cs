0e633d1fc75bafee700e0f3fefc6ef48b72a6a1e
MATEUS OLIVEIRA BRITO <moliveirabrito.institucional@gmail.com>
2022-08-03 11:10:47 -0300

    Cria classe de contexto do banco de dados

---
+++ api_chat_messenger/Database/ChatMessengerDatabaseContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace api_chat_messenger.Database {
+    public class ChatMessengerDatabaseContext : DbContext {
+
+        public ChatMessengerDatabaseContext(DbContextOptions<ChatMessengerDatabaseContext> options) : base(options) {
+
+        }
+    }
+}
