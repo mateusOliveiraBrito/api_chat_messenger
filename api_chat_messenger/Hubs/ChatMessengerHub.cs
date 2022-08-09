6a19ab827233d915c77fcb64766b64f738b4f1c0
MATEUS OLIVEIRA BRITO <moliveirabrito.institucional@gmail.com>
2022-08-09 15:01:51 -0300

    Implementa método para realizar login do usuário

--- api_chat_messenger/Hubs/ChatMessengerHub.cs
+++ api_chat_messenger/Hubs/ChatMessengerHub.cs
@@ -27,5 +27,16 @@ namespace api_chat_messenger.Hubs {
 
             await Clients.Caller.SendAsync("ConfirmarCadastro", true, novoUsuario, "Cadastro realizado com sucesso");
         }
+
+        public async Task RealizarLogin(Usuario usuarioLogin) {
+            var usuarioLogado = _databaseContext.Usuarios.FirstOrDefault(usuario => usuario.Email == usuarioLogin.Email && usuario.Senha == usuarioLogin.Senha);
+
+            if (usuarioLogado == null) {
+                await Clients.Caller.SendAsync("ConfirmarLogin", false, null, "Login não realizado: email e/ou senha estão incorretos");
+                return;
+            }
+
+            await Clients.Caller.SendAsync("ConfirmarLogin", true, usuarioLogado, string.Empty);
+        }
     }
 }
