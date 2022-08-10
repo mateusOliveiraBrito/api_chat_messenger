48a86f93903a8ccc4046b047d4427a5da93c188a
MATEUS OLIVEIRA BRITO <moliveirabrito.institucional@gmail.com>
2022-08-10 14:12:28 -0300

    Altera as propriedades da classe

--- api_chat_messenger/Models/Mensagem.cs
+++ api_chat_messenger/Models/Mensagem.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace api_chat_messenger.Models {
     public class Mensagem {
         public int Id { get; set; }
         public string NomeGrupo { get; set; }
-        public string Usuario { get; set; }
+        public int UsuarioId { get; set; }
+        public string UsuarioJson { get; set; }
+        [NotMapped]
+        public Usuario Usuario { get; set; }
         public string Texto { get; set; }
         public DateTime DataCriacao { get; set; }
     }
